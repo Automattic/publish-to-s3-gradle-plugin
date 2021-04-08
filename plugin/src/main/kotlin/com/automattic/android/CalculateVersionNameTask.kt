b6bbd32e371bd8db9f0c9e9b6d78eae8c2439a0c
Oguz Kocer <oguz.kocer@automattic.com>
2021-04-08 16:07:14 -0400

    Improve CalculateVersionNameTask argument handling

--- plugin/src/main/kotlin/com/automattic/android/CalculateVersionNameTask.kt
+++ plugin/src/main/kotlin/com/automattic/android/CalculateVersionNameTask.kt
@@ -7,35 +7,52 @@ import org.gradle.api.tasks.options.Option
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 
+private const val branchNameArgumentName = "branch-name"
+private const val sha1ArgumentName = "sha1"
+
+private val requiredArgumentsErrorMessage: String by lazy {
+    "--$branchNameArgumentName={branch-name} --$sha1ArgumentName={sha1-commit-hash}" +
+        " command line arguments are required"
+}
+
 open class CalculateVersionNameTask : DefaultTask() {
     @Input
-    @Option(option = "tagName",
+    @Option(option = "tag-name",
         description = "The name of the git tag, if the current build is tagged")
     var tagName: String = ""
 
     @Input
-    @Option(description = "The name of the Git branch currently being built")
-    lateinit var branchName: String
+    @Option(option = branchNameArgumentName, description = "The name of the Git branch currently being built")
+    var branchName: String = ""
 
     @Input
-    @Option(option = "sha1", description = "The SHA1 hash of the last commit of the current build")
-    lateinit var sha1: String
+    @Option(option = sha1ArgumentName, description = "The SHA1 hash of the last commit of the current build")
+    var sha1: String = ""
 
     @Input
-    @Option(description = "The URL of the associated pull request")
-    lateinit var pullRequestUrl: String
+    @Option(option = "pull-request-url", description = "The URL of the associated pull request")
+    var pullRequestUrl: String = ""
 
     @TaskAction
     fun process() {
-        val buildEnvironment = BuildEnvironment(
+        val buildEnvironment = validateArguments()
+
+        // TODO: Improve this error message
+        val versionName = buildEnvironment.calculateVersionName()
+            ?: throw IllegalStateException("Don't publish")
+        println("${versionName}")
+    }
+
+    private fun validateArguments(): BuildEnvironment {
+        require(branchName.isNotEmpty() && sha1.isNotEmpty()) {
+            requiredArgumentsErrorMessage
+        }
+
+        return BuildEnvironment(
             tagName = tagName,
             branchName = branchName,
             sha1 = sha1,
             pullRequestUrl = pullRequestUrl
         )
-        // TODO: Improve this error message
-        val versionName = buildEnvironment.calculateVersionName()
-            ?: throw IllegalStateException("Don't publish")
-        println("${versionName}")
     }
 }
