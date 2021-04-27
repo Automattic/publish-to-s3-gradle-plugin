ff1e79cc20166d34b51627593469765b217f73b0
Oguz Kocer <oguz.kocer@automattic.com>
2021-04-26 22:58:57 -0400

    Refactor CheckS3VersionTask to make it reusable

--- plugin/src/main/kotlin/com/automattic/android/CheckS3VersionTask.kt
+++ plugin/src/main/kotlin/com/automattic/android/CheckS3VersionTask.kt
@@ -1,7 +1,6 @@
 package com.automattic.android.publish
 
-import java.net.HttpURLConnection
-import java.net.URL
+import com.automattic.android.publish.CheckS3Version
 
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -9,58 +8,22 @@ import org.gradle.api.tasks.options.Option
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 
-private const val SUCCESS_STATUS_CODE = 200
-private const val FAILURE_STATUS_CODE = 403
-
 abstract class CheckS3VersionTask : DefaultTask() {
     @Internal
     override fun getDescription(): String = "Checks if a version is already published to S3"
 
-    @Input
-    @Option(option = "published-group-id", description = "Group id used to publish the artifacts")
-    var publishedGroupId: String = ""
+    @get:Input
+    abstract var publishedGroupId: String
 
-    @Input
-    @Option(option = "module-name", description = "Module name of the published artifacts")
-    var moduleName: String = ""
+    @get:Input
+    abstract var moduleName: String
 
-    @Input
-    @Option(option = "version-name", description = "Version name to be checked")
-    var versionName: String = ""
-
-    private val pomUrl: String by lazy {
-        "https://a8c-libs.s3.amazonaws.com/android/${publishedGroupId.replace(".", "/")}/" +
-            "$moduleName/$versionName/$moduleName-${versionName}.pom"
-    }
+    @get:Input
+    abstract var versionName: String
 
     @TaskAction
     fun process() {
-        when (responseCodeForUrl(pomUrl)) {
-            SUCCESS_STATUS_CODE -> println("true")
-            FAILURE_STATUS_CODE -> println("false")
-            else -> handleUnexpectedResponseCode()
-        }
-    }
-
-    private fun responseCodeForUrl(url: String): Int =
-        with(URL(url).openConnection() as HttpURLConnection) {
-            requestMethod = "HEAD"
-            connect()
-            responseCode
-        }
-
-    private fun handleUnexpectedResponseCode() {
-        throw IllegalStateException(
-            "An unexpected response code received with the following information. " +
-                "Please contact Platform 9 team if this issue persists.\n\n" +
-
-                "--Input--\n" +
-                "publishedGroupId: $publishedGroupId\n" +
-                "moduleName: $moduleName\n" +
-                "versionName: $versionName\n\n" +
-
-                "--Calculated--\n" +
-                "POM url: $pomUrl\n"
-        )
+        val result = CheckS3Version(publishedGroupId, moduleName, versionName).check()
+        println("$result")
     }
 }
