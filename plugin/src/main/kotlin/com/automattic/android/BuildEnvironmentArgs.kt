3e8de133dacae04fe8c453f4b69ee27a6e560f2b
Oguz Kocer <oguz.kocer@automattic.com>
2021-05-06 01:37:32 -0400

    Fix BuildEnvironmentArgs argument names

--- plugin/src/main/kotlin/com/automattic/android/BuildEnvironmentArgs.kt
+++ plugin/src/main/kotlin/com/automattic/android/BuildEnvironmentArgs.kt
@@ -2,12 +2,17 @@ package com.automattic.android.publish
 
 import com.automattic.android.publish.BuildEnvironment
 
+const val ARG_TAG_NAME = "tag-name"
+const val ARG_BRANCH_NAME = "branch-name"
+const val ARG_SHA1 = "sha1"
+const val ARG_PR_URL = "pull-request-url"
+
 private val requiredArgumentsErrorMessage: String by lazy {
     "Either tagName or both branch name and sha1 command line arguments needs to be provided!\n" +
     "Example usages:\n" +
-    "--tagName={tag-name}\n" +
-    "--pull-request-url={pull-request-url} --branch-name={branch-name} --sha1={sha1-commit-hash}\n" +
-    "--branch-name={branch-name} --sha1={sha1-commit-hash}"
+    "--$ARG_TAG_NAME={tag-name}\n" +
+    "--$ARG_PR_URL={pull-request-url} --$ARG_BRANCH_NAME={branch-name} --$ARG_SHA1={sha1-commit-hash}\n" +
+    "--$ARG_BRANCH_NAME={branch-name} --$ARG_SHA1={sha1-commit-hash}"
 }
 
 data class BuildEnvironmentArgs(
--- plugin/src/main/kotlin/com/automattic/android/PublishToS3Task.kt
+++ plugin/src/main/kotlin/com/automattic/android/PublishToS3Task.kt
@@ -22,22 +22,19 @@ abstract class PublishToS3Task : DefaultTask() {
     abstract var moduleName: String
 
     @Input
-    @Option(option = "tag-name",
-        description = "The name of the git tag, if the current build is tagged")
+    @Option(option = ARG_TAG_NAME, description = "The name of the git tag, if the current build is tagged")
     var tagName: String = ""
 
     @Input
-    @Option(option = BRANCH_NAME_ARGUMENT_NAME,
-        description = "The name of the Git branch currently being built")
+    @Option(option = ARG_BRANCH_NAME, description = "The name of the Git branch currently being built")
     var branchName: String = ""
 
     @Input
-    @Option(option = SHA1_ARGUMENT_NAME,
-        description = "The SHA1 hash of the last commit of the current build")
+    @Option(option = ARG_SHA1, description = "The SHA1 hash of the last commit of the current build")
     var sha1: String = ""
 
     @Input
-    @Option(option = "pull-request-url", description = "The URL of the associated pull request")
+    @Option(option = ARG_PR_URL, description = "The URL of the associated pull request")
     var pullRequestUrl: String = ""
 
     @TaskAction
