862036f78dc3f6e83c9893c027b020bf9919263f
Oguz Kocer <oguz.kocer@automattic.com>
2021-05-19 11:43:55 -0400

    Simplify functional test runner

--- plugin/src/functionalTest/kotlin/com/automattic/android/FunctionalTestRunnerHelper.kt
+++ plugin/src/functionalTest/kotlin/com/automattic/android/FunctionalTestRunnerHelper.kt
@@ -3,20 +3,23 @@ package com.automattic.android.publish
 import java.io.File
 import org.gradle.testkit.runner.GradleRunner
 
-fun publishToS3HelpersPluginFunctionalTestRunnerWithArguments(vararg arguments: String): GradleRunner {
+fun helpersPluginFunctionalTestRunner(vararg arguments: String): GradleRunner =
+    functionalTestRunner("com.automattic.android.publish-to-s3-helpers", arguments.toList())
+
+private fun functionalTestRunner(pluginId: String, arguments: List<String>): GradleRunner {
     val projectDir = File("build/functionalTest")
     projectDir.mkdirs()
     projectDir.resolve("settings.gradle").writeText("")
     projectDir.resolve("build.gradle.kts").writeText("""
             plugins {
-                id("com.automattic.android.publish-to-s3-helpers")
+                id("$pluginId")
             }
         """)
 
     val runner = GradleRunner.create()
     runner.forwardOutput()
     runner.withPluginClasspath()
-    runner.withArguments(arguments.toList())
+    runner.withArguments(arguments)
     runner.withProjectDir(projectDir)
     return runner
 }
