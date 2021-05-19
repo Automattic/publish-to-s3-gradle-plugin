4a8af07ffe0101a28715747aa4156f97cc9ae4b3
Oguz Kocer <oguz.kocer@automattic.com>
2021-05-19 11:18:38 -0400

    Update detekt to 1.17.0

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     id("org.jetbrains.kotlin.jvm") version "1.4.32"
-    id("io.gitlab.arturbosch.detekt").version("1.16.0")
+    id("io.gitlab.arturbosch.detekt").version("1.17.0")
     id("maven-publish")
 }
 
