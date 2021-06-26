ca8c66563db290fa47a67a56b94b25b92b9a1195
Oguz Kocer <oguz.kocer@automattic.com>
2021-06-26 02:16:54 -0400

    Remove plugin extensions

--- plugin/src/main/kotlin/com/automattic/android/PublishLibraryToS3Extension.kt
+++
@@ -1,8 +0,0 @@
-package com.automattic.android.publish
-
-interface PublishLibraryToS3Extension {
-    var groupId: String
-    var artifactId: String
-    var from: String?
-}
-
--- plugin/src/main/kotlin/com/automattic/android/PublishPluginToS3Extension.kt
+++
@@ -1,7 +0,0 @@
-package com.automattic.android.publish
-
-interface PublishPluginToS3Extension {
-    var groupId: String
-    var artifactId: String
-}
-
