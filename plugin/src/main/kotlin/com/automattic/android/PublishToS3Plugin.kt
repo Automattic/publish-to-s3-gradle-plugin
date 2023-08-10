22ccda292e139676896d977eeb5c1af8afff479f
Oguz Kocer <oguz.kocer@automattic.com>
2023-08-10 18:24:50 -0400

    Adds `publish-to-s3-without-sources` plugin

--- plugin/src/main/kotlin/com/automattic/android/PublishToS3Plugin.kt
+++ plugin/src/main/kotlin/com/automattic/android/PublishToS3Plugin.kt
@@ -1,37 +1,48 @@
 package com.automattic.android.publish
 
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.publish.maven.tasks.GenerateMavenPom
 import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
-import org.gradle.jvm.tasks.Jar
-import com.android.build.gradle.LibraryExtension
 
 class PublishToS3Plugin : Plugin<Project> {
     override fun apply(project: Project) {
-        project.plugins.apply("maven-publish")
-        project.addS3MavenRepository()
+        applyInternal(project)
+    }
+}
 
-        project.tasks.register("calculateVersionName", CalculateVersionNameTask::class.java)
-        project.tasks.register("isVersionPublishedToS3", CheckS3VersionTask::class.java)
+class PublishToS3WithoutSourcesPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        applyInternal(project, withSources = false)
+    }
+}
 
-        project.pluginManager.withPlugin("com.android.library") {
-            project.extensions.findByType(LibraryExtension::class.java)?.let { androidLibrary ->
-                androidLibrary.publishing.singleVariant("release") {
+private fun applyInternal(project: Project, withSources: Boolean = true) {
+    project.plugins.apply("maven-publish")
+    project.addS3MavenRepository()
+
+    project.tasks.register("calculateVersionName", CalculateVersionNameTask::class.java)
+    project.tasks.register("isVersionPublishedToS3", CheckS3VersionTask::class.java)
+
+    project.pluginManager.withPlugin("com.android.library") {
+        project.extensions.findByType(LibraryExtension::class.java)?.let { androidLibrary ->
+            androidLibrary.publishing.singleVariant("release") {
+                if (withSources) {
                     withSourcesJar()
-                    withJavadocJar()
                 }
+                withJavadocJar()
             }
         }
-
-        val preTask = project.tasks.register("prepareToPublishToS3", PrepareToPublishToS3Task::class.java)
-        project.tasks.withType(PublishToMavenRepository::class.java) { task ->
-            task.dependsOn(preTask)
-        }
-        project.tasks.withType(GenerateMavenPom::class.java) { task ->
-            task.dependsOn(preTask)
-        }
-
-        project.printPublishedVersionNameAfterPublishTasks()
     }
+
+    val preTask = project.tasks.register("prepareToPublishToS3", PrepareToPublishToS3Task::class.java)
+    project.tasks.withType(PublishToMavenRepository::class.java) { task ->
+        task.dependsOn(preTask)
+    }
+    project.tasks.withType(GenerateMavenPom::class.java) { task ->
+        task.dependsOn(preTask)
+    }
+
+    project.printPublishedVersionNameAfterPublishTasks()
 }
